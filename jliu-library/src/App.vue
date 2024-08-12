7296553eebeb24b823130db646b5cf33661189f5
Jiayin Liu <jliu0326@student.monash.edu>
2024-08-12 13:51:56 +1000

    week3

--- jliu-library/src/App.vue
+++ jliu-library/src/App.vue
@@ -2,20 +2,22 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import JSON from './components/JSON.vue';
+import Form  from './components/Form.vue';
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
-  <main>
+  <!-- <main>
     <JSON />
-  </main>
+  </main> -->
+  <Form />
 </template>
 
 <style scoped>
